556c0e74da975da04d0a9d0ac719c06148a6892c
Olufemi Emmanuel Oladimeji <oladimejiemmanuel801@gmail.com>
2026-03-12 14:54:25 +0100

    first commit

---
+++ src/components/Homecards.vue
@@ -0,0 +1,37 @@
+<script setup>
+import CardView from "./CardView.vue";
+</script>
+
+<template>
+  <section class="py-4">
+    <div class="container-xl lg:container m-auto">
+      <div class="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg">
+        <CardView>
+          <h2 class="text-2xl font-bold">For Developers</h2>
+          <p class="mt-2 mb-4">
+            Browse our Vue jobs and start your career today
+          </p>
+          <Routerlink
+            to="/jobs"
+            class="inline-block bg-black text-white rounded-lg px-4 py-2 hover:bg-gray-700"
+          >
+            Browse Jobs
+          </Routerlink>
+        </CardView>
+
+        <CardView bg="bg-green-100">
+          <h2 class="text-2xl font-bold">For Employers</h2>
+          <p class="mt-2 mb-4">
+            List your job to find the perfect developer for the role
+          </p>
+          <Routerlink
+            to="/jobs/add"
+            class="inline-block bg-green-500 text-white rounded-lg px-4 py-2 hover:bg-green-600"
+          >
+            Add Job
+          </Routerlink>
+        </CardView>
+      </div>
+    </div>
+  </section>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<script setup>
+import HeroView from '../components/HeroView.vue';
+import HomeCards from '../components/Homecards.vue';
+import JobsListings from '../components/JobsListings.vue';
+</script>
+
+
+<template>
+    <HeroView />
+  <HomeCards />
+  <JobsListings :limit="3" :show-button=true />
+</template>
