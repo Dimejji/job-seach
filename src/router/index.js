e9e038b288e9af5de9c0b2cbb98457912af73e93
Olufemi Emmanuel Oladimeji <oladimejiemmanuel801@gmail.com>
2026-03-23 11:35:32 +0100

    uppdatted

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import JobsView from "../views/JobsView.vue";
 import JobView from "../views/JobView.vue";
-import EditJobView from "../views/EditJobView.vue";
+import EditJobView from "../views/EditJobview.vue"
 import AddJobView from "../views/AddJobView.vue";
 import NotFoundView from "../views/NotFoundView.vue";
 
